64ecfbc638711284899e1acba4acf676bc164e1f
Rashad Chowdhury <78812401+rashad-moves@users.noreply.github.com>
2022-01-13 13:06:37 -0500

    add skeleton for luhn.sh

--- bash/luhn/luhn.sh
+++ bash/luhn/luhn.sh
@@ -1,1 +1,45 @@
 #!/usr/bin/env bash
+
+main() {
+  local result="false"
+  local number="$1"
+  local total=0
+  
+  number=strip_leading_zeros number
+  number=strip_whitespace number
+  
+  check_preconditions number
+  
+  (( total % 10 == 0 )) && result="true"
+
+  echo "${result}"
+}
+
+strip_leading_zeros() {
+
+}
+
+strip_whitespace() {
+
+}
+
+check_preconditions() {
+  must_be_longer_than_single_digit "$1"
+  must_contain_only_numbers "$1"
+}
+
+
+must_contain_only_numbers() {
+  
+}
+
+must_be_longer_than_single_digit() {
+  
+}
+
+fail() {
+  echo "false"
+  exit 0
+}
+
+main "$1"
