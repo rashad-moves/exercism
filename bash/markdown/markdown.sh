666f8a1ebcca7a67c3e36194c5e83eb39dc84ebf
Rashad Chowdhury <78812401+rashad-moves@users.noreply.github.com>
2022-02-05 08:19:55 -0500

    spacing

--- bash/markdown/markdown.sh
+++ bash/markdown/markdown.sh
@@ -16,53 +16,53 @@ add_bold_html() {
 while IFS= read -r line; do
   add_bold_html
 
-        echo "$line" | grep '^\*' > /dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            if [ X$inside_a_list != Xyes ]; then
-                h="$h<ul>"
-                inside_a_list=yes
-            fi
+  echo "$line" | grep '^\*' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    if [ X$inside_a_list != Xyes ]; then
+        h="$h<ul>"
+        inside_a_list=yes
+    fi
 
-                while [[ $line == *_*?_* ]]; do
-                  one=${line#*_}
-                  two=${one#*_}
-                    if [ ${#two} -lt ${#one} -a ${#one} -lt ${#line} ]; then
-                      line="${line%%_$one}<em>${one%%_$two}</em>$two"
-                    fi
-                done
-            h="$h<li>${line#??}</li>"
+    while [[ $line == *_*?_* ]]; do
+      one=${line#*_}
+      two=${one#*_}
+        if [ ${#two} -lt ${#one} -a ${#one} -lt ${#line} ]; then
+          line="${line%%_$one}<em>${one%%_$two}</em>$two"
+        fi
+    done
+    h="$h<li>${line#??}</li>"
 
-        else
+  else
     if [ X$inside_a_list = Xyes ]; then
         h="$h</ul>"
         inside_a_list=no
     fi
 
-            n=`expr "$line" : "#\{1,\}"`
-            if [ $n -gt 0 -a 7 -gt $n ]; then
+    n=`expr "$line" : "#\{1,\}"`
+    if [ $n -gt 0 -a 7 -gt $n ]; then
 
-        while [[ $line == *_*?_* ]]; do
-            s=${line#*_}
-            t=${s#*_}
-            if [ ${#t} -lt ${#s} -a ${#s} -lt ${#line} ]; then
-                line="${line%%_$s}<em>${s%%_$t}</em>$t"
-            fi
-        done
-                HEAD=${line:n}
-                while [[ $HEAD == " "* ]]; do HEAD=${HEAD# }; done
-                h="$h<h$n>$HEAD</h$n>"
+      while [[ $line == *_*?_* ]]; do
+          s=${line#*_}
+          t=${s#*_}
+          if [ ${#t} -lt ${#s} -a ${#s} -lt ${#line} ]; then
+              line="${line%%_$s}<em>${s%%_$t}</em>$t"
+          fi
+      done
+      HEAD=${line:n}
+      while [[ $HEAD == " "* ]]; do HEAD=${HEAD# }; done
+      h="$h<h$n>$HEAD</h$n>"
 
-            else
+    else
 
-        grep '_..*_' <<<"$line" > /dev/null &&
-            line=`echo "$line" | sed -E 's,_([^_]+)_,<em>\1</em>,g'`
-                h="$h<p>$line</p>"
-            fi
-        fi
-    done < "$1"
-
-    if [ X$inside_a_list = Xyes ]; then
-        h="$h</ul>"
+      grep '_..*_' <<<"$line" > /dev/null &&
+        line=`echo "$line" | sed -E 's,_([^_]+)_,<em>\1</em>,g'`
+      h="$h<p>$line</p>"
     fi
+  fi
+done < "$1"
 
-    echo "$h"
+if [ X$inside_a_list = Xyes ]; then
+  h="$h</ul>"
+fi
+
+echo "$h"
